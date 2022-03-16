9b92ac1719fdbb8d286c782d31e9f069ffba3a33
llifezou <llifezou@gmail.com>
2022-03-16 20:17:25 +0800

    Remove 'github.com/shirou/gopsutil/disk' compatible with macOS 12.0

--- hdwal.go
+++ hdwal.go
@@ -2,28 +2,26 @@ package hdwallet
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/binary"
 	"io"
-	"crypto/sha256"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/net"
-	"github.com/tyler-smith/go-bip39"
 	"math"
 	"strconv"
 	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
+	"github.com/tyler-smith/go-bip39"
 )
 
 func entropy() ([]byte, error) {
 	randomBytes := make([]byte, 0)
 	cpuPercent, _ := cpu.Percent(time.Second, false)
 	memory, _ := mem.VirtualMemory()
-	diskStatus, _ := disk.Usage("/")
 
 	ioCounters, _ := net.IOCounters(true)
 	netWork := strconv.Itoa(int(ioCounters[0].BytesSent + ioCounters[0].BytesRecv))
-
 
 	cRandBytes := make([]byte, 32)
 	if _, err := io.ReadFull(rand.Reader, cRandBytes); err != nil {
@@ -33,14 +31,13 @@ func entropy() ([]byte, error) {
 	randomBytes = append(randomBytes, cRandBytes...)
 	randomBytes = append(randomBytes, float64ToByte(cpuPercent[0])...)
 	randomBytes = append(randomBytes, float64ToByte(memory.UsedPercent)...)
-	randomBytes = append(randomBytes, float64ToByte(diskStatus.UsedPercent)...)
 	randomBytes = append(randomBytes, []byte(netWork)...)
 
 	random := sha256.Sum256(randomBytes)
 	return random[:16], nil
 }
 
-func NewMnemonic() (string, error){
+func NewMnemonic() (string, error) {
 	entropyBytes, err := entropy()
 	if err != nil {
 		return "", err
@@ -52,7 +49,6 @@ func NewMnemonic() (string, error){
 	}
 	return mnemonic, nil
 }
-
 
 func GenerateSeedFromMnemonic(mnemonic, password string) ([]byte, error) {
 	seedBytes, err := bip39.NewSeedWithErrorChecking(mnemonic, password)
@@ -85,11 +81,9 @@ func GetExtendSeedFromPath(path string, seed []byte) ([]byte, error) {
 	return extendedKey.key, nil
 }
 
-
 func float64ToByte(float float64) []byte {
 	bits := math.Float64bits(float)
 	bytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(bytes, bits)
 	return bytes
 }
-
